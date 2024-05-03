e184fcb83372af1c728d943cb97ab2af252cb5f6
Mario Torres <scaramuzzini@gmail.com>
2024-05-03 10:12:18 -0400

    Import inicial

---
+++ app/src/main/java/com/oceanbrasil/aprendendonotificacoes/NotificationHelper.kt
@@ -0,0 +1,52 @@
+package com.oceanbrasil.aprendendonotificacoes
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class NotificationHelper(private val context: Context) {
+
+    fun criarCanalDeNotificacao() {
+        val name = "Ocean Notificacoes"
+        val descriptionText = "Canal oficial de notificacoes OCEAN"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system.
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun criarNotificacao() {
+        var builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Ocean Aula Push Notification")
+            .setContentText("Esta e' uma aula de push notification no Ocean/UEA")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(context)) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+            }
+            // notificationId is a unique int for each notification that you must define.
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "ocean_notification_channel"
+        const val NOTIFICATION_ID = 101
+//        var TEMP = "OK"
+    }
+}
